6a31928cf3d26a2152057b66dfff47b721433beb
LitoMore <litomore@gmail.com>
2018-01-07 23:37:03 +0800

    Remove unused variable

--- fanfou.js
+++ fanfou.js
@@ -8,7 +8,7 @@ const Fanfou = {};
 Fanfou.upload = (token, filePath) => {
 	return new Promise(resolve => {
 		const ff = new FanfouSDK(token);
-		ff.up('/photos/upload', {photo: fs.createReadStream(filePath), status: '刚刚用 Kap 发布了一张照片'}, (err, res) => {
+		ff.up('/photos/upload', {photo: fs.createReadStream(filePath), status: '刚刚用 Kap 发布了一张照片'}, err => {
 			if (err) {
 				resolve(err.message);
 			} else {
