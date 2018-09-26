fefbd377450f266423d7b40540f33a180e64ce48
LitoMore <litomore@gmail.com>
2018-09-27 00:50:08 +0800

    Bump ava@1.0.0-beta.8

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ const path = require('path');
 const test = require('ava');
 const kapPluginTest = require('kap-plugin-test');
 
-test(async t => {
+test('test plugin', async t => {
 	const config = {
 		consumerKey: 'key',
 		consumerSecret: 'secret',
@@ -13,12 +13,9 @@ test(async t => {
 	};
 	const plugin = kapPluginTest(path.join(__dirname, 'fixtures/unicorn.gif'), {config});
 
-	try {
-		await plugin.run();
-	} catch (error) {
-		delete plugin.context.config.s.useNofanConfig;
+	delete plugin.context.config.s.useNofanConfig;
+	t.deepEqual(config, plugin.context.config.s);
 
-		t.deepEqual(config, plugin.context.config.s);
-		t.is(error.message, 'Invalid consumer');
-	}
+	const error = await t.throwsAsync(plugin.run);
+	t.is(error.message, 'Invalid consumer');
 });
